52dc280d7ea94b3582170f6b077b4a11a907f2d4
Dhanu Agnihotri <dhanuagnihotri@gmail.com>
2015-02-10 18:00:08 -0800

    Initial Commit

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Dhanu Agnihotri on 2/10/15.
+//  Copyright (c) 2015 ___SocietyTech___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yelp
+//
+//  Created by Dhanu Agnihotri on 2/10/15.
+//  Copyright (c) 2015 ___SocietyTech___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
